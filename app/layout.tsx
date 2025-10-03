8479d52328d5e31bd92f59836863093c3dca3020
Brady <woznia93@msu.edu>
2025-10-03 15:00:59 -0400

    updates to titles

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
-    template: '%s | Next.js Portfolio Starter',
+    default: 'Brady Wozniak Portfolio',
+    template: '%s | Brady Wozniak Portfolio',
   },
   description: 'This is my portfolio.',
   openGraph: {
