168d69e6b042f6895f45b837efc7c524cfc64bc5
Brady <woznia93@msu.edu>
2025-11-24 14:32:02 -0500

    added sorucing to git

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -46,7 +46,7 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://vercel.com/templates/next.js/portfolio-starter-kit"
+            href="https://github.com/woznia93/bw"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">view source</p>
